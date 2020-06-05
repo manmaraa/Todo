bc3573879401be3e9f1c67d2bf98e8216277b31f
manmaraa <63203549+manmaraa@users.noreply.github.com>
2020-06-05 13:54:50 +0200

    Add files via upload

---
+++ demo/src/main/java/com/WebApp/WebAppFeladatApplication.java
@@ -0,0 +1,13 @@
+package com.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppFeladatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppFeladatApplication.class, args);
+	}
+
+}
